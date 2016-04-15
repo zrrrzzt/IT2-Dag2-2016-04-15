1ffad9a4fa5dbaf3bc807beafa52efd0385291f9
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-04-15 10:13:02 +0200

    Updates with html example

---
+++ basic-view.js
@@ -0,0 +1,55 @@
+'use strict'
+
+// Lag en liste som inneholder tallene 1,6,3,2,10,5,12,15,6,9,17,14,7
+
+var liste = [1, 6, 3, 2, 10, 5, 12, 15, 6, 9, 17, 14, 7]
+var listeLengde = liste.length
+var totalSum = 0
+
+// Skriv ut alle tallene i lista
+console.log('Skriv ut alle tallene i lista')
+
+for (var i = 0; i < listeLengde; i++) {
+  console.log(liste[i])
+}
+
+// Skriv ut alle tall som er større enn 10
+console.log('Skriv ut alle tall som er større enn 10')
+
+for (var i = 0; i < listeLengde; i++) {
+  if (liste[i] > 10) {
+    console.log(liste[i])
+  }
+}
+
+// Skriv ut alle tall som er delelig med 2
+console.log('Skriv ut alle tall som er delelig med 2')
+
+for (var i = 0; i < listeLengde; i++) {
+  if (liste[i] % 2 === 0) {
+    console.log(liste[i])
+  }
+}
+
+// Skriv ut tallene i omvendt rekkefølge
+console.log('Skriv ut tallene i omvendt rekkefølge')
+
+liste.reverse()
+
+for (var i = 0; i < listeLengde; i++) {
+  console.log(liste[i])
+}
+
+// Skriv ut summen av tallene i lista
+console.log('Skriv ut summen av tallene i lista')
+
+for (var i = 0; i < listeLengde; i++) {
+  totalSum += liste[i]
+}
+
+console.log(totalSum)
+
+// Skriv ut gjennomsnittsverdien av tallene i lista
+console.log('Skriv ut gjennomsnittsverdien av tallene i lista')
+
+console.log(totalSum/listeLengde)
